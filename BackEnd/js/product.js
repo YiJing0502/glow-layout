d9e6941487e105c32e334cbaaa2cfd2b3c45db12
YiJing0502 <w71080635@gmail.com>
2024-01-22 13:21:28 +0800

    元件化Modal

--- BackEnd/js/product.js
+++ BackEnd/js/product.js
@@ -6,37 +6,227 @@ let myDeleModal;
 let myResultModal;
 // 分頁元件
 const PageBtn = {
-  data(){
-    return{
-    }
-  },
   props: ['page', 'currentPage'],
   emits: ['change-page'],
-  mounted(){
-    console.log()
-  },
   template: `<li class="page-item"><button type="button" class="page-link" :class="{active: currentPage == page}" :data-page="page"  @click="$emit('change-page', page)">{{page}}</button></li>`
 };
 const PrevPageBtn = {
-  data(){
-    return{
-      
-    };
-  },
   props: ['isEnabled',],
   emits: ['change-page',],
   template: `<li class="page-item"><button type="button" class="page-link" :class="{disabled: !isEnabled,}" @click="$emit('change-page')">Previous</button></li>`,
 };
 const NextPageBtn = {
-  data(){
-    return{
-      
-    };
-  },
   props: ['isEnabled',],
   emits: ['change-page',],
   template: `<li class="page-item"><button type="button" class="page-link" :class="{disabled: !isEnabled,}" @click="$emit('change-page')">Next</button></li>`,
 };
+// 刪除 Modal 元件
+const DeleteModal = {
+  methods:{
+    deleteAdminProduct(){
+      this.$emit('delete-admin-product');
+    },
+  },
+  props: ['showData',],
+  emits: ['delete-admin-product',],
+  template: `<div class="modal fade" id="bsDeleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header bg-main-spec text-main-light">
+        <h3 class="modal-title fs-5" id="exampleModalLabel">
+          刪除產品
+        </h3>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>是否刪除<strong class="text-main-spec">{{showData.title}}</strong>？</p>
+        <p>刪除後將無法恢復</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-normal-gray" data-bs-dismiss="modal">關閉</button>
+        <button type="button" class="btn btn-normal-spec" @click="deleteAdminProduct">刪除此產品</button>
+      </div>
+    </div>
+  </div>
+</div>`
+};
+// 結果 Modal 元件
+const ResultModal = {
+  props: ['serverMessage',],
+  template: `<div class="modal fade" id="bsResultModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"
+  tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">
+          {{ serverMessage.success ? '成功' : '失敗'}}
+        </h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        {{ serverMessage.message }}
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-normal-gray" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">關閉</button>
+      </div>
+    </div>
+  </div>
+</div>`,
+};
+// 產品 Modal 元件
+const ProductModal = {
+  methods: {
+    uploadImages(){
+      this.$emit('upload-images')
+    },
+    addImage(){
+      this.$emit('add-image');
+    },
+    putAdminProduct(id){
+      this.$emit('put-admin-product', id);
+    },
+    postAdminProduct(){
+      this.$emit('post-admin-product');
+    }
+  },
+  props: ['showData', 'inEditProductMode'],
+  emits: ['upload-images', 'add-image', 'put-admin-product', 'post-admin-product'],
+  template: `<div class="modal fade" id="bsProductModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+  aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">{{inEditProductMode ? '編輯產品' : '新增產品'}}</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <nav>
+          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <button class="nav-link active w-50" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home"
+              type="button" role="tab" aria-controls="nav-home" aria-selected="true">商品內容</button>
+            <button class="nav-link w-50" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile"
+              type="button" role="tab" aria-controls="nav-profile" aria-selected="false">商品圖片</button>
+          </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab"
+            tabindex="0">
+            <div class="row mt-3">
+              <div class="row">
+                <div class="col-10">
+                  <div class="mb-3">
+                    <label for="title" class="form-label">標題</label>
+                    <input type="text" class="form-control" id="title" placeholder="請輸入標題" v-model="showData.title">
+                  </div>
+                </div>
+                <div class="col-2 d-flex align-items-center">
+                  <div class="form-check mt-3">
+                    <input type="checkbox" class="form-check-input" id="is_enabled" placeholder="請輸入標題"
+                      :true-value="1" :false-value="0" v-model="showData.is_enabled">
+                    <label for="is_enabled" class="form-check-label">是否啟用</label>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="mb-3 col">
+                  <label for="category" class="form-label">分類</label>
+                  <input type="text" class="form-control" id="category" placeholder="請輸入分類"
+                    v-model="showData.category">
+                </div>
+                <div class="mb-3 col">
+                  <label for="unit" class="form-label">單位</label>
+                  <input type="text" class="form-control" id="unit" placeholder="請輸入單位" v-model="showData.unit">
+                </div>
+              </div>
+              <div class="row">
+                <div class="mb-3 col">
+                  <label for="original_price" class="form-label">原價</label>
+                  <input type="number" class="form-control" id="original_price" placeholder="請輸入原價"
+                    v-model.number="showData.origin_price">
+                </div>
+                <div class="mb-3 col">
+                  <label for="price" class="form-label">售價</label>
+                  <input type="number" class="form-control" id="price" placeholder="請輸入售價"
+                    v-model.number="showData.price">
+                </div>
+              </div>
+              <div class="row">
+                <div class="mb-3 col">
+                  <label for="cost_price" class="form-label">成本價</label>
+                  <input type="number" class="form-control" id="cost_price" placeholder="請輸入成本價"
+                    v-model.number="showData.cost_price">
+                </div>
+                <div class="mb-3 col">
+                  <label for="inventory" class="form-label">庫存量</label>
+                  <input type="number" class="form-control" id="inventory" placeholder="請輸入庫存量"
+                    v-model.number="showData.inventory">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <div class="mb-3">
+                    <label for="description" class="form-label">產品描述</label>
+                    <textarea class="form-control" id="description" rows="3"
+                      v-model="showData.description"></textarea>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="mb-3">
+                    <label for="content" class="form-label">說明內容</label>
+                    <textarea class="form-control" id="content" rows="3" v-model="showData.content"></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab"
+            tabindex="0">
+            <div class="row row-cols-4 mt-3">
+              <div class="col mb-3">
+                <div class="mb-3">
+                  <label for="imageUrl" class="form-label">輸入主要圖片網址</label>
+                  <input type="text" class="form-control" placeholder="請輸入主要圖片連結" v-model="showData.imageUrl">
+                </div>
+                <img :src="showData.imageUrl" :alt="showData.title" class="img-fluid">
+              </div>
+              <div class="col mb-3" v-for="(item,index) in showData.imagesUrl">
+                <div class="mb-3">
+                  <label for="imageUrl" class="form-label">輸入其他圖片網址</label>
+                  <button type="button" class="btn-close float-end" aria-label="Close" @click="deleteImage(index)">
+                  </button>
+                  <input type="text" class="form-control" placeholder="請輸入其他圖片連結"
+                    v-model="showData.imagesUrl[index]">
+                </div>
+                <img v-if="showData.imagesUrl[index]" :src="showData.imagesUrl[index]" :alt="showData.title+index"
+                  class="img-fluid">
+              </div>
+              <div class="col mb-3">
+                <div class="card">
+                  <div class="card-body">
+                    <label for="formFile" class="form-label">上傳圖片檔案</label>
+                    <input class="form-control" type="file" ref="myImgUploadInput" @change="uploadImages"
+                      accept="image/jpeg, image/jpg, image/png" multiple>
+                    <hr>
+                    <button class="btn btn-solid-gray w-100" @click="addImage">手動新增圖片</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-normal-gray" data-bs-dismiss="modal">關閉</button>
+        <button type="button" class="btn btn-solid-spec"
+          @click="inEditProductMode ? putAdminProduct(showData.id) : postAdminProduct()">{{inEditProductMode ? '更新'
+          : '新增'}}</button>
+      </div>
+    </div>
+  </div>
+</div>`,
+
+}
 const app = createApp({
   data() {
     return {
@@ -56,6 +246,9 @@ const app = createApp({
     PageBtn,
     PrevPageBtn,
     NextPageBtn,
+    DeleteModal,
+    ResultModal,
+    ProductModal,
   },
   methods: {
     // modal, 打開編輯產品modal
@@ -232,11 +425,9 @@ const app = createApp({
           };
         })
         .catch(err=>{
-          if(!err.data.success){
-            this.serverMessage.message = err.response.data.message;
-            this.serverMessage.success = err.response.data.success;
-            myResultModal.show();
-          }
+          this.serverMessage.message = err.response.data.message;
+          this.serverMessage.success = err.response.data.success;
+          myResultModal.show();
         });
     },
     // ajax, 刪除特定產品資料

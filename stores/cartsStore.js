e21a86e9b9952244fa9d89bc3bd3c1e3afef9ade
YiJing0502 <w71080635@gmail.com>
2024-01-29 10:33:43 +0800

    新增cartsStore

---
+++ stores/cartsStore.js
@@ -0,0 +1,13 @@
+const { defineStore } = Pinia;
+import { baseUrl, apiPath } from '../views/config.js';
+export default defineStore('cartsStore', {
+  state: ()=>({
+
+  }),
+  getters: {
+
+  },
+  actions: {
+
+  },
+});
